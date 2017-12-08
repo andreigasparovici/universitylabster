f26ea95d4755642a5b2611747e48d44ae1f6fb43
Andrei Gasparovici <andreigasparovici1@gmail.com>
2017-12-08 16:30:26 +0200

    Added login and register activities

--- app/src/main/java/com/kernelpanic/universitylabster/LoginActivity.java
+++ app/src/main/java/com/kernelpanic/universitylabster/LoginActivity.java
@@ -1,9 +1,31 @@
 package com.kernelpanic.universitylabster;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import butterknife.BindView;
+import butterknife.OnClick;
 
 public class LoginActivity extends AppCompatActivity {
+
+    @BindView(R.id.editEmail)
+    EditText editEmail;
+
+    @BindView(R.id.editPassword)
+    EditText editPassword;
+
+    @OnClick(R.id.loginButton)
+    void doLogin() {
+        /// ....
+    }
+
+    public void goToRegister(View view) {
+        startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+        finish();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
